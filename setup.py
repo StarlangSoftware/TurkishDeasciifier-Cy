3a04a2158ce75e24283922e552fabe7d875beb1f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-10-07 09:10:26 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     ext_modules=cythonize(["Deasciifier/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-Deasciifier-Cy',
-    version='1.0.1',
+    version='1.0.2',
     packages=['Deasciifier'],
     package_data={'Deasciifier': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/olcaytaner/TurkishDeasciifier-Cy',
