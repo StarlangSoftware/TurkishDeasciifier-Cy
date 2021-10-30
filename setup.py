978f8248da8dfab8e1da18a4eb641967763f5ac6
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 21:54:16 +0300

    Updated setup.py and added dictionary.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["Deasciifier/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-Deasciifier-Cy',
-    version='1.0.6',
+    version='1.0.7',
     packages=['Deasciifier'],
     package_data={'Deasciifier': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/TurkishDeasciifier-Cy',
@@ -13,5 +17,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Asciifier/Deasciifier Library',
-    install_requires=['NlpToolkit-MorphologicalAnalysis-Cy', 'NlpToolkit-NGram-Cy']
+    install_requires=['NlpToolkit-MorphologicalAnalysis-Cy', 'NlpToolkit-NGram-Cy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
