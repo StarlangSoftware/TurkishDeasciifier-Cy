47b82268de435f8194698418bfb8617698222216
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-22 22:25:04 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["Deasciifier/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-Deasciifier-Cy',
-    version='1.0.7',
+    version='1.0.8',
     packages=['Deasciifier'],
     package_data={'Deasciifier': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/TurkishDeasciifier-Cy',
